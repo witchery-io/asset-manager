3af7ff1d4ab7022be43efeab1de2717f13e78927
edo.g <Eduard2018$>
2018-10-16 13:35:55 +0400

    have changed components structure

--- src/app/components/settings/balance/balance.component.ts
+++ src/app/components/settings/balance/balance.component.ts
@@ -1,68 +1,11 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges, TemplateRef } from '@angular/core';
-import { OrderService } from '../../../services/order.service';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-balance',
   templateUrl: './balance.component.html',
   styleUrls: ['./balance.component.scss'],
 })
-export class BalanceComponent implements OnInit, OnChanges {
+export class BalanceComponent {
 
-  modalRef: BsModalRef;
-  @Input() values: any;
-
-  selectedOrder: number;
-  selectedPosition: number;
-  orderType = ['buy', 'sell'];
-  orders = [];
-
-  currentOrderTab: number;
-
-  constructor(
-    private orderService: OrderService,
-    private modalService: BsModalService,
-  ) { }
-
-  ngOnChanges(changes: SimpleChanges) {
-    this.fetchOrders();
-  }
-
-  ngOnInit() {
-    this.selectTab(0);
-    this.fetchOrders();
-  }
-
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
-  }
-
-  selectTab(tab_id: number) {
-    this.currentOrderTab = tab_id;
-  }
-
-  fetchOrders() {
-    this.orderService.getGroupOrders(this.values.id, {})
-      .subscribe(
-        orders => {
-          this.orders = orders;
-        }
-      );
-  }
-
-  modify() {
-    // code ...
-  }
-
-  confirm(): void {
-    console.log('Confirmed!');
-    console.log(this.currentOrderTab);
-    this.modalRef.hide();
-  }
-
-  decline(): void {
-    console.log('Declined!');
-    console.log(this.currentOrderTab);
-    this.modalRef.hide();
-  }
+  @Input() balance: any;
 }
---
+++ src/app/components/settings/order/order.component.ts
@@ -0,0 +1,68 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges, TemplateRef } from '@angular/core';
+import { OrderService } from '../../../services/order.service';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.scss'],
+})
+export class OrderComponent implements OnInit, OnChanges {
+
+  modalRef: BsModalRef;
+  @Input() order: any;
+
+  selectedOrder: number;
+  selectedPosition: number;
+  orderType = ['buy', 'sell'];
+  orders = [];
+
+  currentOrderTab: number;
+
+  constructor(
+    private orderService: OrderService,
+    private modalService: BsModalService,
+  ) { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.fetchOrders();
+  }
+
+  ngOnInit() {
+    this.selectTab(0);
+    this.fetchOrders();
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
+  }
+
+  selectTab(tab_id: number) {
+    this.currentOrderTab = tab_id;
+  }
+
+  fetchOrders() {
+    this.orderService.getGroupOrders(this.order.id, {})
+      .subscribe(
+        orders => {
+          this.orders = orders;
+        }
+      );
+  }
+
+  modify() {
+    // code ...
+  }
+
+  confirm(): void {
+    console.log('Confirmed!');
+    console.log(this.currentOrderTab);
+    this.modalRef.hide();
+  }
+
+  decline(): void {
+    console.log('Declined!');
+    console.log(this.currentOrderTab);
+    this.modalRef.hide();
+  }
+}
