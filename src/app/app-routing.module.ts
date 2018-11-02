246ac8a6743aac551ef2b57f6692a20742dd0f8f
edo.g <Eduard2018$>
2018-11-02 12:52:05 +0400

    nav menu - bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,23 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {MainComponent} from './components/main/main.component';
-import {SettingsComponent} from './components/settings/settings.component';
-import {TradingComponent} from './components/trading/trading.component';
-import {BotsComponent} from './components/bots/bots.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
+import { NgModule} from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+import { MainComponent} from './components/main/main.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { TradingComponent } from './components/trading/trading.component';
+import { BotsComponent } from './components/bots/bots.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: '', component: MainComponent},
+  { path: '', component: MainComponent },
   {
-    path: 'dashboard', component: DashboardComponent, children: [
-      {path: '', redirectTo: 'settings', pathMatch: 'full'},
-      {path: 'settings', component: SettingsComponent},
-      {path: 'bots', component: BotsComponent},
-      {path: 'trading', redirectTo: 'trading/group/6a86df61-c190-4347-9b61-34cbd88d38a4'},
-      {path: 'trading/:type/:id', component: TradingComponent},
-    ]
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      { path: '', redirectTo: 'trading/group/6a86df61-c190-4347-9b61-34cbd88d38a4', pathMatch: 'full' },
+      { path: 'trading', redirectTo: 'trading/group/6a86df61-c190-4347-9b61-34cbd88d38a4' },
+      { path: 'trading/:type/:id', component: TradingComponent },
+      { path: 'settings', component: SettingsComponent },
+      { path: 'bots', component: BotsComponent },
+    ],
   },
 ];
 
