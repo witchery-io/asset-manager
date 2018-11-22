b8f1911b6e5385472c11e630f6aee6f701a05269
edo.g <Eduard2018$>
2018-11-22 13:43:29 +0400

    str. 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
-
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-import {
-  TabsModule, ModalModule, CollapseModule, AlertModule, TooltipModule
-} from 'ngx-bootstrap';
-
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { NotifierModule } from 'angular-notifier';
 
 import { AppComponent } from './app.component';
-import * as components from './components';
-import * as containers from './containers';
+import { DashboardComponent } from './containers';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -26,38 +14,12 @@ library.add(fas);
 @NgModule({
   declarations: [
     AppComponent,
-    containers.DashboardComponent,
-    components.MainComponent,
-    components.SettingsComponent,
-    components.TradingComponent,
-    components.BotsComponent,
-    components.GroupsComponent,
-    components.AccountsComponent,
-    components.BalanceComponent,
-    components.OrderComponent,
-    components.TemplateComponent,
-    components.BotComponent,
-    components.BotOrderComponent,
-    components.OrderItemComponent,
-    components.StatusBarComponent,
-    components.TvChartComponent,
-    components.ButtonViewComponent,
+    DashboardComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     CoreModule,
-    ModalModule.forRoot(),
-    CollapseModule.forRoot(),
-    TabsModule.forRoot(),
-    AlertModule.forRoot(),
-    TooltipModule.forRoot(),
-    FontAwesomeModule,
-    ReactiveFormsModule,
-    FormsModule,
-    HttpClientModule,
     NgxSpinnerModule,
-    Ng2SmartTableModule,
     NotifierModule.withConfig({
       position: {
         horizontal: {
@@ -65,9 +27,6 @@ library.add(fas);
         },
       },
     }),
-  ],
-  entryComponents: [
-    components.ButtonViewComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,58 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import * as services from '../services';
+import * as components from '../components';
+import {
+  TabsModule,
+  ModalModule,
+  CollapseModule,
+  AlertModule,
+  TooltipModule,
+} from 'ngx-bootstrap';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  imports: [],
-  exports: [],
-  declarations: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    Ng2SmartTableModule,
+    FontAwesomeModule,
+    TabsModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    CollapseModule.forRoot(),
+    AlertModule.forRoot(),
+  ],
+  exports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    Ng2SmartTableModule,
+    FontAwesomeModule,
+  ],
+  declarations: [
+    components.MainComponent,
+    components.ButtonViewComponent,
+    components.SettingsComponent,
+    components.TradingComponent,
+    components.BotsComponent,
+    components.GroupsComponent,
+    components.AccountsComponent,
+    components.BalanceComponent,
+    components.OrderComponent,
+    components.TemplateComponent,
+    components.BotComponent,
+    components.BotOrderComponent,
+    components.OrderItemComponent,
+    components.StatusBarComponent,
+    components.TvChartComponent,
+  ],
   providers: [
     services.AccountService,
     services.GroupsService,
@@ -16,6 +64,8 @@ import * as services from '../services';
     services.BotService,
     services.NotifierService,
   ],
-  entryComponents: [],
+  entryComponents: [
+    components.ButtonViewComponent,
+  ],
 })
 export class CoreModule { }
