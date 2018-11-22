d1bf3035d42e2c53ef98b347be46960a8b09ab9d
edo.g <Eduard2018$>
2018-11-22 13:47:55 +0400

    str. 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
-import {
-  MainComponent,
-  SettingsComponent,
-  TradingComponent,
-  BotsComponent,
-} from './components';
-import {
-  DashboardComponent
-} from './containers';
-
-
+import * as components from './components';
+import { DashboardComponent } from './containers';
 
 const routes: Routes = [
-  { path: '', component: MainComponent },
+  { path: '', component: components.MainComponent },
   {
     path: 'dashboard',
     component: DashboardComponent,
     children: [
       { path: '', redirectTo: 'trading/group/6a86df61-c190-4347-9b61-34cbd88d38a4', pathMatch: 'full' },
       { path: 'trading', redirectTo: 'trading/group/6a86df61-c190-4347-9b61-34cbd88d38a4' },
-      { path: 'trading/:type/:id', component: TradingComponent },
-      { path: 'settings', component: SettingsComponent },
-      { path: 'bots', component: BotsComponent },
+      { path: 'trading/:type/:id', component: components.TradingComponent },
+      { path: 'settings', component: components.SettingsComponent },
+      { path: 'bots', component: components.BotsComponent },
     ],
   },
 ];
