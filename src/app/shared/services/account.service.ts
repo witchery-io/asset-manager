6896acf4bfd99e6a402ee0ebd1e0c6cd61da03bb
edo.g <Eduard2018$>
2018-12-04 17:24:27 +0400

    code ... 7

--- src/app/shared/services/account.service.ts
+++ src/app/shared/services/account.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AccountService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getAccounts(): Observable<any> {
+    return this.http.get(`http://trade.vitanova.online:50089/accounts/accounts`);
+  }
 }
--- src/app/shared/services/group.service.ts
+++ src/app/shared/services/group.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class GroupService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getGroups(): Observable<any> {
+    return this.http.get(`http://trade.vitanova.online:50089/accounts/groups`);
+  }
 }
