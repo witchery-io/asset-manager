c485cd17966494a0704110150685d48ef544b1b9
edo.g <Eduard2018$>
2018-12-05 19:40:56 +0400

    ref. 16

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,6 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { TradingModule } from '@trading/trading.module';
-import { BotsModule } from '@bots/bots.module';
-import { SettingsModule } from '@settings/settings.module';
-
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
