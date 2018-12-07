cc07902b40420efa7853179d04dd377b8a9b0154
edo.g <Eduard2018$>
2018-12-07 12:36:59 +0400

    rxjs

--- src/app/manager/trading/trading.module.ts
+++ src/app/manager/trading/trading.module.ts
@@ -4,22 +4,12 @@ import { TradingRoutingModule } from './trading-routing.module';
 import { TradingComponent } from '@trading/containers';
 import { SharedModule } from '@app/shared/shared.module';
 
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { AccountEffects } from '@app/core/effects/account.effects';
-import { GroupEffects } from '@app/core/effects/group.effects';
-import { reducers } from '@app/core/reducers';
-
 @NgModule({
   declarations: [
     TradingComponent,
   ],
   imports: [
     SharedModule,
-
-    StoreModule.forFeature('trading', reducers),
-    EffectsModule.forFeature([AccountEffects, GroupEffects]),
-
     TradingRoutingModule,
   ]
 })
