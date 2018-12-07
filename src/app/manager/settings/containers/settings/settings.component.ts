9781a52933259f6fb06c0e0ac3e47027d67320ee
edo.g <Eduard2018$>
2018-12-07 12:35:33 +0400

    rxjs

--- src/app/manager/settings/containers/settings/settings.component.ts
+++ src/app/manager/settings/containers/settings/settings.component.ts
@@ -9,7 +9,6 @@ export class SettingsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
--- src/app/manager/trading/containers/trading/trading.component.ts
+++ src/app/manager/trading/containers/trading/trading.component.ts
@@ -1,13 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-
-import * as SelectAccount from '@trading/state/account.selectors';
-import * as SelectGroup from '@trading/state/group.selectors';
-import { LoadAccounts } from '@app/core/actions/account.actions';
-import { Observable } from 'rxjs';
-import { Account, Group } from '@app/core/intefaces';
-import { LoadGroups } from '@app/core/actions/group.actions';
-import { TradingState } from '@app/core/reducers';
 
 @Component({
   selector: 'app-trading',
@@ -16,28 +7,7 @@ import { TradingState } from '@app/core/reducers';
 })
 export class TradingComponent implements OnInit {
 
-  accounts$: Observable<Account[]>;
-  isLoadingAccount$: Observable<boolean>;
-  errorAccount$: Observable<string>;
+  constructor() {}
 
-  groups$: Observable<Group[]>;
-  isLoadingGroup$: Observable<boolean>;
-  errorGroup$: Observable<string>;
-
-  constructor(
-    public store: Store<TradingState>,
-  ) {
-    this.accounts$ = this.store.pipe(select(SelectAccount.getAccounts));
-    this.isLoadingAccount$ = this.store.pipe(select(SelectAccount.isLoading));
-    this.errorAccount$ = this.store.pipe(select(SelectAccount.getError));
-
-    this.groups$ = this.store.pipe(select(SelectGroup.getGroups));
-    this.isLoadingGroup$ = this.store.pipe(select(SelectGroup.isLoading));
-    this.errorGroup$ = this.store.pipe(select(SelectGroup.getError));
-  }
-
-  ngOnInit() {
-    this.store.dispatch(new LoadAccounts());
-    this.store.dispatch(new LoadGroups());
-  }
+  ngOnInit() {}
 }
