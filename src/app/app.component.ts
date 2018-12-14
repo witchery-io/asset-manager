a08b47e8b9c3b514e18649f17f4f24760ed07c7e
edo.g <Eduard2018$>
2018-12-14 11:47:52 +0400

    socket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { LoadGroups } from '@app/core/actions/group.actions';
-import { LoadAccounts } from '@app/core/actions/account.actions';
-import { AppState } from '@app/core/intefaces/app-state.interface';
-import { LoadTicks } from '@app/core/actions/tick.actions';
 
 @Component({
   selector: 'app-root',
