ce12ae248d50f17cf20e5fa7bacdbfd9ebb5f94a
edo.g <Eduard2018$>
2018-12-14 18:39:28 +0400

    trading && settings - bug fixed

--- src/app/manager/settings/state/settings.selectors.ts
+++ src/app/manager/settings/state/settings.selectors.ts
@@ -4,10 +4,10 @@ import { SettingsState, State } from '@settings/reducers';
 /*
 * Create SETTINGS selectors
 * */
-export const getTrading = (state: State) => state.settings;
+export const getSettings = (state: State) => state.settings;
 
-export const getOrders = createSelector(getTrading, (state: SettingsState) => state.orders);
+export const getOrders = createSelector(getSettings, (state: SettingsState) => state.orders);
 
-export const getPositions = createSelector(getTrading, (state: SettingsState) => state.positions);
+export const getPositions = createSelector(getSettings, (state: SettingsState) => state.positions);
 
-export const getBalance = createSelector(getTrading, (state: SettingsState) => state.balance);
+export const getBalance = createSelector(getSettings, (state: SettingsState) => state.balance);
