1748912316a49aa5c879eba45139e4ffb3a26ac7
edo.g <Eduard2018$>
2018-12-14 18:05:42 +0400

    trading && settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { LoadGroups } from '@app/core/actions/group.actions';
+import { LoadAccounts } from '@app/core/actions/account.actions';
+import { AppState } from '@app/core/intefaces/app-state.interface';
+import { LoadTicks } from '@app/core/actions/tick.actions';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +12,12 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   constructor(
+    private store: Store<AppState>,
   ) {}
 
   ngOnInit() {
-
+    this.store.dispatch(new LoadGroups());
+    this.store.dispatch(new LoadAccounts());
+    this.store.dispatch(new LoadTicks());
   }
 }
