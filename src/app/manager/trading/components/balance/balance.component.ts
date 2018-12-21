c00449a878014702cdeb5dbf02e49899a85570ce
edo.g <Eduard2018$>
2018-12-21 13:38:01 +0400

    test

--- src/app/manager/trading/components/balance/balance.component.ts
+++ src/app/manager/trading/components/balance/balance.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef } from '@angular/core';
 import { getBalanceFromSection } from '@trading/state/trading.selectors';
+import { getAccountsFromSection, getGroupsFromSection } from '@app/core/reducers';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { OrdersService } from '@app/shared/services';
 
 @Component({
   selector: 'app-balance',
@@ -11,14 +15,63 @@ export class BalanceComponent implements OnInit {
   @Input()
   section: any;
 
-  constructor() {
+  @Input()
+  groupSection: any;
+
+  @Input()
+  accountsSection: any;
+
+  modalRef: BsModalRef;
+  transferForm: FormGroup;
+  curr_balance: any;
+  currency: any;
+
+  constructor(
+    private ordersService: OrdersService,
+    private modalService: BsModalService,
+    private fb: FormBuilder,
+  ) {
   }
 
   ngOnInit() {
-    console.log(this);
+    this.transferForm = this.fb.group({
+      group: new FormControl(''),
+      account: new FormControl(''),
+      market1: new FormControl('', [<any>Validators.required]),
+      market2: new FormControl('', [<any>Validators.required]),
+      amount: new FormControl(0, [<any>Validators.required]),
+    });
   }
 
   get balance() {
     return getBalanceFromSection(this.section);
+  }
+
+  get accounts() {
+    return getAccountsFromSection(this.accountsSection);
+  }
+
+  get groups() {
+    return getGroupsFromSection(this.groupSection);
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  chooseMarker(market) {
+    this.curr_balance = market;
+  }
+
+  onTransfer(model: any, is_Valid: boolean) {
+    if (is_Valid) {
+      const model2 = { ...model, ...{ currency: this.currency } };
+      console.log(model2);
+      this.modalRef.hide();
+    }
+  }
+
+  chooseCurrency(curr) {
+    this.currency = curr;
   }
 }
