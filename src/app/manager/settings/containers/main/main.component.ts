f68de8fe88831531e631a0feeb8673ff93d2ce9e
edo.g <Eduard2018$>
2019-01-17 11:54:33 +0400

    test

--- src/app/manager/settings/containers/main/main.component.ts
+++ src/app/manager/settings/containers/main/main.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { OrderTab, TypeTab } from '@app/shared/enums';
 import { TabsetComponent } from 'ngx-bootstrap';
-import { GROUPS } from '@app/shared/enums/trading.enum';
 import { select, Store } from '@ngrx/store';
 import * as Select from '@settings/state/settings.selectors';
 import { SettingsState } from '@settings/reducers';
@@ -13,12 +12,7 @@ import * as fromBalance from '@settings/reducers/balance.reducers';
 import * as fromAccounts from '@app/core/reducers/account.reducers';
 import * as fromGroups from '@app/core/reducers/group.reducers';
 import * as fromTicks from '@app/core/reducers/tick.reducers';
-import { SettingsSet, SettingsUpdate } from '@settings/actions/settings.actions';
-import { LoadBalance } from '@settings/actions/balance.actions';
-import { LoadOrders } from '@settings/actions/orders.actions';
-import { LoadPositions } from '@settings/actions/positions.actions';
-import { LoadGroup } from '@settings/actions/group.actions';
-import { LoadAccount } from '@settings/actions/account.actions';
+import { ACCOUNTS, GROUPS } from '@app/shared/enums/trading.enum';
 
 @Component({
   selector: 'app-trading',
@@ -26,7 +20,10 @@ import { LoadAccount } from '@settings/actions/account.actions';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-  @ViewChild('typeTabs') typeTabs: TabsetComponent;
+  GROUPS = GROUPS;
+  ACCOUNTS = ACCOUNTS;
+
+  @ViewChild('generalTabs') generalTabs: TabsetComponent;
   @ViewChild('ordersTabs') ordersTabs: TabsetComponent;
 
   orders$: Observable<fromOrders.State>;
@@ -43,15 +40,26 @@ export class MainComponent implements OnInit {
   ticks$: Observable<fromTicks.State>;
   ticksIsLoading$: Observable<boolean>;
 
-  type$: Observable<string>;
-  id$: Observable<string>;
-  accountId$: Observable<string>;
-
   group$: Observable<any>;
   account$: Observable<any>;
 
   _defaultTabIndex = 0;
   groupByPair = false;
+
+  /**
+   * 3 param of url ex. accounts, groups
+   */
+  type: string;
+
+  /*
+  * General Tab
+  * */
+  generalTab: string;
+
+  /*
+  * Order Tab
+  * */
+  orderTab: string;
 
   constructor(
     private store: Store<SettingsState>,
@@ -73,80 +81,45 @@ export class MainComponent implements OnInit {
     this.ticks$ = this.store.pipe(select(Select.getTicks));
     this.ticksIsLoading$ = this.store.pipe(select(Select.ticksIsLoading));
 
-    this.type$ = this.store.pipe(select(Select.getType));
-    this.id$ = this.store.pipe(select(Select.getId));
-    this.accountId$ = this.store.pipe(select(Select.getAccountId));
-
     this.group$ = this.store.pipe(select(Select.getGroup));
     this.account$ = this.store.pipe(select(Select.getAccount));
   }
 
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id');
-    const accountId = this.route.snapshot.paramMap.get('accountId');
-    const type = this.route.snapshot.paramMap.get('type');
-    const active_tab = this.route.snapshot.paramMap.get('tab');
+    this.route.params.subscribe(params => {
+      /*
+      * Set Type
+      * */
+      this.type = params.type;
 
-    /*
-    * Set active tab
-    * */
-    this.ordersTabs.tabs[OrderTab[active_tab] || this._defaultTabIndex].active = true;
-    this.typeTabs.tabs[TypeTab[type] || this._defaultTabIndex].active = true;
+      /*
+      * Set tabs from url
+      * */
+      this.generalTab = params.generalTab;
+      this.orderTab = params.orderTab;
 
-    /*
-    * Set current trading id and type
-    * */
-    this.store.dispatch(new SettingsSet({
-      id: id,
-      accountId: accountId,
-      type: type,
-      groupByPair: this.groupByPair,
-    }));
-
-    this.store.dispatch(new LoadGroup('6a86df61-c190-4347-9b61-34cbd88d38a4'));
-    this.store.dispatch(new LoadAccount('edc23b04-64d8-4469-bb6a-40da55322d26'));
-
-    /*
-    * Load data
-    * */
-    this.store.dispatch(new LoadBalance({
-      id: id,
-      type: type,
-      groupByPair: this.groupByPair,
-    }));
-    this.store.dispatch(new LoadOrders({
-      id: id,
-      type: type,
-      groupByPair: this.groupByPair,
-    }));
-    this.store.dispatch(new LoadPositions({
-      id: id,
-      type: type,
-      groupByPair: this.groupByPair,
-    }));
-  }
-
-  onSelectTypeTab(type_tab) {
-    const order_tab = this.route.snapshot.paramMap.get('tab');
-    const typePromise = this._navigate(type_tab, order_tab);
-
-    typePromise.then(() => {
-      this.typeTabs.tabs[TypeTab[type_tab] || this._defaultTabIndex].active = true;
+      /*
+      * Set active tabs
+      * */
+      this.generalTabs.tabs[TypeTab[params.generalTab] || this._defaultTabIndex].active = true;
+      this.ordersTabs.tabs[OrderTab[params.orderTab] || this._defaultTabIndex].active = true;
     });
   }
 
-  onSelectOrderTab(tab_id) {
-    const orderPromise = this.router.navigate([`../${tab_id}`], {relativeTo: this.route});
-
-    orderPromise.then(() => {
-      this.ordersTabs.tabs[OrderTab[tab_id] || this._defaultTabIndex].active = true;
-    });
+  /**
+   *
+   * @param name ex. groups, accounts
+   */
+  onSelectGeneralTab(name: string) {
+    const orderTab = this.route.snapshot.paramMap.get('orderTab');
+    this.router.navigate([`../../${name}/${orderTab}`], {relativeTo: this.route});
   }
 
-  private _navigate(type_tab, order_tab) {
-    this.store.dispatch(new SettingsUpdate({type: type_tab}));
-
-    const id = type_tab === GROUPS ? '6a86df61-c190-4347-9b61-34cbd88d38a4' : 'edc23b04-64d8-4469-bb6a-40da55322d26'; // todo :: change
-    return this.router.navigate([`../../../${type_tab}/${id}/${order_tab}/`], {relativeTo: this.route});
+  /**
+   *
+   * @param name string ex. positions, orders
+   */
+  onSelectOrderTab(name: string) {
+    this.router.navigate([`../${name}`], {relativeTo: this.route});
   }
 }
