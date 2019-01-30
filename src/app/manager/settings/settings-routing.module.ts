7e0b3e1372c262b8c212c7c409fe0b9e2d13977f
edo.g <Eduard2018$>
2019-01-30 19:10:38 +0400

    login structure

--- src/app/manager/settings/settings-routing.module.ts
+++ src/app/manager/settings/settings-routing.module.ts
@@ -4,12 +4,19 @@ import { MainComponent } from '@settings/containers';
 import { AuthGuard } from '@app/shared/services/auth-guard.service';
 
 const routes: Routes = [
-  {path: '', component: MainComponent, canActivate: [AuthGuard]},
-  {path: ':generalTab', component: MainComponent, canActivate: [AuthGuard]},
-  {path: ':generalTab/:id', component: MainComponent, canActivate: [AuthGuard]},
-  {path: ':generalTab/:id/:orderTab', component: MainComponent, canActivate: [AuthGuard]},
-  {path: ':generalTab/:id/:subType/:subId', component: MainComponent, canActivate: [AuthGuard]},
-  {path: ':generalTab/:id/:subType/:subId/:orderTab', component: MainComponent, canActivate: [AuthGuard]},
+  {
+    path: '',
+    component: MainComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {path: ':generalTab', component: MainComponent, canActivate: [AuthGuard]},
+      {path: ':generalTab/:id', component: MainComponent, canActivate: [AuthGuard]},
+      {path: ':generalTab/:id/:orderTab', component: MainComponent, canActivate: [AuthGuard]},
+      {path: ':generalTab/:id/:subType/:subId', component: MainComponent, canActivate: [AuthGuard]},
+      {path: ':generalTab/:id/:subType/:subId/:orderTab', component: MainComponent, canActivate: [AuthGuard]},
+    ],
+  },
+
 ];
 
 @NgModule({
