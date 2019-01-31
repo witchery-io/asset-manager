4144cfecb12e0675dd67192dcc106408f83301e8
edo.g <Eduard2018$>
2019-01-31 11:23:50 +0400

    login structure

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '@app/core/services/api.service';
-import { map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { throwError as observableThrowError } from 'rxjs';
+import { NotifierService } from 'angular-notifier';
 
 @Injectable()
 export class AuthService {
 
   private redirectUrlProp: string;
+  private readonly notifier: NotifierService;
 
   constructor(
     private api: ApiService,
+    private router: Router,
+    private notifierService: NotifierService,
   ) {
+    this.notifier = notifierService;
   }
 
   get redirectUrl(): string {
@@ -34,10 +41,38 @@ export class AuthService {
 
           return res;
         }),
+        catchError(this.handleError.bind(this)),
       );
   }
 
   logout() {
     return this.api.clearAuthKey();
+  }
+
+  /**
+   * Handle server response errors here
+   */
+  private handleError(error: any) {
+    let errMsg = 'Server error';
+    let body;
+
+    if (error && error.status) {
+      if (error.status === 401) {
+        this.api.clearAuthKey();
+        this.router.navigate(['/login']);
+        return observableThrowError(errMsg);
+      }
+    }
+
+    if (error && error._body) {
+      body = JSON.parse(error._body);
+    }
+
+    if (body && body.errors && body.errors.exception && body.errors.exception.message) {
+      errMsg = body.errors.exception.message;
+    }
+
+    this.notifier.notify('error', errMsg);
+    return observableThrowError(errMsg);
   }
 }
