861f43453ec093e4f9be948e5b3466f676547c12
edo.g <Eduard2018$>
2019-02-27 13:39:28 +0400

    ATSP-71 - Allocation multiplier on group order create

--- src/app/shared/components/templates/market/market.component.ts
+++ src/app/shared/components/templates/market/market.component.ts
@@ -1,6 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Role } from '@app/shared/enums';
+import { GroupService } from '@app/core/services';
+import { GROUPS } from '@app/shared/enums/trading.enum';
+import { Group } from '@app/core/intefaces';
 
 @Component({
   selector: 'app-market',
@@ -8,29 +11,38 @@ import { Role } from '@app/shared/enums';
   styleUrls: ['./market.component.scss']
 })
 export class MarketComponent implements OnInit {
-
+  @Input()
+  id: string;
+  @Input()
+  type: string;
   @Input()
   role = 'admin';
-
   @Input()
   values = {};
-
   @Input()
   marketType: string;
-
   @Output()
   order: EventEmitter<any> = new EventEmitter();
-
   marketForm: FormGroup;
-
   ROLE = Role;
-
   orderType = ['stop', 'market', 'limit'];
+  group: Group;
+  total = 0;
 
-  constructor() {
+  constructor(
+    private groupService: GroupService,
+  ) {
   }
 
   ngOnInit() {
+    if (this.type === GROUPS) {
+      this.groupService.getGroup(this.id)
+        .subscribe(val => {
+          this.group = val;
+          this.calcTotal();
+        });
+    }
+
     this.marketForm = new FormGroup({
       type: new FormControl('stop', [<any>Validators.required]),
       price: new FormControl(0),
@@ -57,5 +69,18 @@ export class MarketComponent implements OnInit {
         priceOCOStop: model.priceOCOStop,
       });
     }
+  }
+
+  calcTotal() {
+    if (this.type !== GROUPS || !this.group.accounts) {
+      return;
+    }
+
+    let total = 0;
+    for (const a of this.group.accounts) {
+      total += (a.multiplier || 0) * this.marketForm.value.amount;
+    }
+
+    this.total = total;
   }
 }
