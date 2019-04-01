af3e5680c595b3ecd57e2d5cc2238a6e3ce427a1
edo.g <Eduard2018$>
2019-04-01 17:19:33 +0400

    ticker opt.6

--- src/app/manager/trading/trading-routing.module.ts
+++ src/app/manager/trading/trading-routing.module.ts
@@ -4,7 +4,7 @@ import { MainComponent } from '@trading/containers';
 import { AuthGuard } from '@app/shared/services/auth-guard.service';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'groups/5c586baff771351cf5a89704/positions'},
+  {path: '', redirectTo: 'accounts/5c5a85b6629f282330d961b2/positions'},
   {path: ':type/:id/:tab', component: MainComponent, canActivate: [AuthGuard]},
 ];
 
